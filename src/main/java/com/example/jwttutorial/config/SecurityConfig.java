55372e7ed99226157be4ea8c17bc6b92929dd554
juyeon0815 <wndusx1@gmail.com>
2022-08-30 22:00:57 +0900

    feat : Security 설정, Data 설정

---
+++ src/main/java/com/example/jwttutorial/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.jwttutorial.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**", "/favicon.ico");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() //HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
+                .antMatchers("/api/hello").permitAll() //인증없이 접근 허용
+                .anyRequest().authenticated();
+    }
+}
